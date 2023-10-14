9fb15c12c367e6f7fabb36735efeb80cc2087a2e
jbalthis <balthis.jason@gmail.com>
2023-10-14 09:38:25 +0000

    layout: hero tweaking

--- app/(public)/components/hero.tsx
+++ app/(public)/components/hero.tsx
@@ -29,16 +29,16 @@ const Hero = () => {
         bg-cover
         bg-no-repeat
         bg-center
-        px-8
+        px-4
       `}
       >
         <div
-          className={`flex-1 ${styles.flexStart} flex-col xl:px-4 sm:px-16 px-8 gap-8`}
+          className={`flex-1 ${styles.flexStart} flex-col xl:px-4 sm:px-8 px-2 gap-4`}
         >
           <div
-            className={`flex flex-row items-center py-2 px-2 ${bg_discount_gradient} rounded-[10px] mb-4`}
+            className={`flex flex-row items-center py-4 px-4 ${bg_discount_gradient} rounded-[10px] mb-4`}
           >
-            <AreaChart className="w-[24px] h-[24px] text-white mx-2" />
+            <AreaChart className="sm:w-[25px] sm:h-[25px] w-[70px] h-[70px] text-white mx-2" />
             <p className={`${styles.paragraph} ml-4 text-sm`}>
               <span className="text-white">
                 Over <span className="italic text-lime-300">20 years</span>{' '}
@@ -54,11 +54,11 @@ const Hero = () => {
           </div>
 
           <div className="flex flex-col justify-between gap-2 w-full">
-            <h1 className="font-poppins font-semibold ss:text-[92px] text-[72px] text-gray-900 ss:leading-[100.8px] leading-[75px]">
+            <h1 className="font-poppins font-semibold ss:text-[72px] text-[52px] text-indigo-900 ss:leading-[100.8px] leading-[75px]">
               Trust.
             </h1>
             <h1
-              className={`font-poppins font-semibold ss:text-[92px] text-[72px] ss:leading-[100.8px] leading-[75px] ${text_gradient}`}
+              className={`font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100.8px] leading-[75px] ${text_gradient}`}
             >
               Transparency.
             </h1>
@@ -66,7 +66,7 @@ const Hero = () => {
               Tranquility.
             </h1>
           </div>
-          <div className="ss:flex hidden md:mr-4 mr-8">
+          <div className="ss:flex hidden mt-2">
             <GetStarted />
           </div>
 
@@ -78,7 +78,7 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className={`ss:hidden ${styles.flexCenter} mt-4`}>
+        <div className={`ss:hidden ${styles.flexCenter} px-4`}>
           <GetStarted />
         </div>
       </div>
